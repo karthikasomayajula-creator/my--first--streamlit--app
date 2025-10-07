ad4b28458373d690470046d6556078ed84a763f8
karthikasomayajula-creator <karthika.somayajula@gmail.com>
2025-10-07 15:40:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,13 +7,10 @@ name = st.text_input("Enter your name:")
 if name:
     st.success(f"Welcome, {name}!")
     from openai import OpenAI
-import streamlit as st
 
 with st.sidebar:
     openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
-    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
-    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
-    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
+   
 
 st.title("💬 Chatbot")
 st.caption("🚀 A Streamlit chatbot powered by OpenAI")
