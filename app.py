864da06bbc0e710e75ba6ca28063f4e9cf1c0b22
karthikasomayajula-creator <karthika.somayajula@gmail.com>
2025-10-07 15:57:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,12 +6,10 @@ st.write("Hello, this is my first Streamlit deployment!")
 name = st.text_input("Enter your name:")
 if name:
     st.success(f"Welcome, {name}!")
-    from openAi import openAI
+    from openAI import openAI
 import streamlit as st
 with st.sidebar:
    openAI_api_key = st.text_input("API Key", key="chatbot_api_key", type="password")
-
-
 st.title("💬 Chatbot")
 st.caption("🚀 A Streamlit chatbot powered by OpenAI")
 if "messages" not in st.session_state:
