c974d1e236b64f8de71e172c206056eef95fbb54
karthikasomayajula-creator <karthika.somayajula@gmail.com>
2025-10-11 20:25:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,6 @@
 import streamlit as st
 
-st.title("🌟 My First Streamlit App")
-st.write("Hello, this is my first Streamlit deployment!")
 
-name = st.text_input("Enter your name:")
-if name:
-    st.success(f"Welcome, {name}!")
     from openAI import openAI
 import streamlit as st
 with st.sidebar:
